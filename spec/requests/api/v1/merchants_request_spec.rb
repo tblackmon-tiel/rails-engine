278810371f80e32c3bf6a6c349e81b9657746c60
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-17 23:47:38 -0600

    add a test for merchants show response

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Merchants API" do
-  it "sends a list of merchants" do
+  it "sends a list of merchants from /merchants" do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
@@ -27,5 +27,36 @@ RSpec.describe "Merchants API" do
       expect(attributes).to have_key(:name)
       expect(attributes[:name]).to be_a String
     end
+  end
+
+  it "sends a single merchants details from /merchants/:id" do
+    create_list(:merchant, 3)
+    primary_merchant = Merchant.create!(name: "Primary Merchant")
+
+    get "/api/v1/merchants/#{primary_merchant.id}"
+
+    expect(response).to be_successful
+    merchant = JSON.parse(response.body)
+
+    expect(merchant).to have_key("data")
+    expect(merchant["data"]).to be_a Hash
+
+    data = merchant["data"]
+
+    expect(data).to have_key("id")
+    expect(data["id"]).to be_a String
+    expect(data).to have_key("type")
+    expect(data["type"]).to be_a String
+    expect(data).to have_key("attributes")
+    expect(data["attributes"]).to be_a Hash
+
+    attributes = data["attributes"]
+
+    expect(attributes).to have_key("name")
+    expect(attributes["name"]).to be_a String
+    expect(attributes).to have_key("description")
+    expect(attributes["description"]).to be_a String
+    expect(attributes).to have_key("unit_price")
+    expect(attributes["unit_price"]).to be_a Numeric
   end
 end
