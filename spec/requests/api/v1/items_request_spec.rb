048829484e4aa2ac4d16fae5739243ccb39a9b15
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-18 01:27:55 -0600

    add response and test for items show route

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -33,4 +33,38 @@ RSpec.describe "Items API" do
       expect(attributes["merchant_id"]).to be_a Numeric
     end
   end
+
+  it "returns a single item from /items/:id" do
+    merchant = Merchant.create!(name: "Kiwi")
+    create_list(:item, 10, merchant_id: merchant.id)
+    item = Item.create!(name: "Test Item", description: "just a test", unit_price: 20.32, merchant_id: merchant.id)
+
+    get "/api/v1/items/#{item.id}"
+    expect(response).to be_successful
+
+    details = JSON.parse(response.body)
+    expect(details["data"]).to be_a Hash
+
+    item = details["data"]
+
+    expect(item).to have_key("id")
+    expect(item["id"]).to be_a String
+
+    expect(item).to have_key("type")
+    expect(item["type"]).to be_a String
+    
+    expect(item).to have_key("attributes")
+    expect(item["attributes"]).to be_a Hash
+
+    attributes = item["attributes"]
+
+    expect(attributes).to have_key("name")
+    expect(attributes["name"]).to be_a String
+    expect(attributes).to have_key("description")
+    expect(attributes["description"]).to be_a String
+    expect(attributes).to have_key("unit_price")
+    expect(attributes["unit_price"]).to be_a Numeric
+    expect(attributes).to have_key("merchant_id")
+    expect(attributes["merchant_id"]).to be_a Numeric
+  end
 end
