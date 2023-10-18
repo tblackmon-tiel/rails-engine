2f8c5b63703059168dfb90c146534e2aa78e853d
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-17 22:25:55 -0600

    add additional data to merchants index test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,5 +7,25 @@ RSpec.describe "Merchants API" do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants).to have_key(:data)
+    expect(merchants[:data]).to be_an Array
+
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a String
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_a String
+      
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to be_a Hash
+
+      attributes = merchant[:attributes]
+
+      expect(attributes).to have_key(:name)
+      expect(attributes[:name]).to be_a String
+    end
   end
 end
