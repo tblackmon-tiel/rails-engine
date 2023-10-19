0f12428f4117b10578b027a01a3c08123f73ce27
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-19 17:36:42 -0600

    finalize testing

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -258,6 +258,45 @@ RSpec.describe "Items API" do
     end
   end
 
+  it "can find all items by a given price range (min and max)" do
+    merchant = Merchant.create!(name: "Kiwi")
+    item1 = Item.create!(name: "searchable One", description: "a searchable item", unit_price: 1023, merchant_id: merchant.id)
+    item2 = Item.create!(name: "SEARCHAble Two", description: "a searchable item", unit_price: 1, merchant_id: merchant.id)
+    item3 = Item.create!(name: "SeARchable Three", description: "a searchable item", unit_price: 23, merchant_id: merchant.id)
+    item4 = Item.create!(name: "No match one", description: "a non-searchable item", unit_price: 50.40, merchant_id: merchant.id)
+    item5 = Item.create!(name: "no match TWO", description: "a non-searchable item", unit_price: 10943.2, merchant_id: merchant.id)
+  
+    get "/api/v1/items/find_all?min_price=1&max_price=50"
+    expect(response).to be_successful
+  
+    items = JSON.parse(response.body)
+    expect(items).to have_key("data")
+    expect(items["data"]).to be_an Array
+    expect(items["data"].count).to eq(2)
+  
+    items["data"].each do |item|
+      expect(item).to have_key("id")
+      expect(item["id"]).to be_a String
+  
+      expect(item).to have_key("type")
+      expect(item["type"]).to be_a String
+      
+      expect(item).to have_key("attributes")
+      expect(item["attributes"]).to be_a Hash
+  
+      attributes = item["attributes"]
+  
+      expect(attributes).to have_key("name")
+      expect(attributes["name"]).to be_a String
+      expect(attributes).to have_key("description")
+      expect(attributes["description"]).to be_a String
+      expect(attributes).to have_key("unit_price")
+      expect(attributes["unit_price"]).to be_a Numeric
+      expect(attributes).to have_key("merchant_id")
+      expect(attributes["merchant_id"]).to be_a Numeric
+    end
+  end
+
   describe "sad paths" do
     it "returns 404 on an id that doesn't exist" do
       get "/api/v1/items/999999999"
@@ -308,6 +347,16 @@ RSpec.describe "Items API" do
       expect(data).to have_key(:errors)
       expect(data[:errors]).to be_a String
       expect(data[:errors]).to eq("Name and price parameters cannot be requested at the same time")
+    end
+
+    it "returns an error if the request includes a min or max price below 0" do
+      get "/api/v1/items/find_all?max_price=-1"
+      expect(response).to_not be_successful
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data).to have_key(:errors)
+      expect(data[:errors]).to be_a String
+      expect(data[:errors]).to eq("Min and max price must be a positive number")
     end
   end
 
