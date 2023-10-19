9378c5075df4f46f0c68688348e4cf1ca2243af9
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-19 12:19:15 -0600

    add tests for item model methods, update item search controller to include multiple params

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::Items::SearchController < ApplicationController
   def index
-    render json: ItemSerializer.new(Item.find_all_by_name(params[:name]))
+    if params[:name]
+      render json: ItemSerializer.new(Item.find_all_by_name(params[:name]))
+    elsif params[:min_price] && params[:max_price]
+      render json: ItemSerializer.new(Item.find_all_by_price_range)
+    elsif params[:min_price]
+      render json: ItemSerializer.new(Item.find_all_by_min_price)
+    elsif params[:max_price]
+      render json: ItemSerializer.new(Item.find_all_by_max_price)
+    end
   end
 end
