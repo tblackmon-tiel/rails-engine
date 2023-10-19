671885ccb2a31cb2de58b13b1f28bd0e928a02d5
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-19 01:51:38 -0600

    add find_by_name method to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,10 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
+
+  def self.find_by_name(name)
+    Merchant.where("name ilike ?", "%" + Merchant.sanitize_sql_like(name) + "%")
+            .order("name")
+            .take
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :invoices }
     it { should have_many :items }
   end
+
+  describe "class methods" do
+    describe "#find_by_name" do
+      it "returns the first merchant, alphabetically, with an alike name case insensitive" do
+        merchant = Merchant.create!(name: "mY SeARch MERchanT")
+        create_list(:merchant, 15)
+
+        expect(Merchant.find_by_name("search")).to eq(merchant)
+      end
+
+      it "handles multiple similar results" do
+        merchant1 = Merchant.create!(name: "Kiwibird")
+        merchant2 = Merchant.create!(name: "A kiwi bird")
+        merchant3 = Merchant.create!(name: "Ckiwi")
+
+        expect(Merchant.find_by_name("KIWI")).to eq(merchant2)
+      end
+    end
+  end
 end
