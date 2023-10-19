8a2e66fe74d179d504d741101f30e15693f29a51
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-19 01:21:18 -0600

    refactor: update merchants items controller routing and remove model method

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,4 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   belongs_to :merchant
-
-  def self.by_merchant(id)
-    Item.where("merchant_id = ?", id)
-  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,22 +7,4 @@ RSpec.describe Item, type: :model do
     
     it { should belong_to :merchant }
   end
-
-  describe "class methods" do
-    before(:each) do
-      @merchant1 = Merchant.create!(name: "Kiwi Merchant")
-      @merchant2 = Merchant.create!(name: "Chicken Merchant")
-      create_list(:item, 3, merchant_id: @merchant1.id)
-      create_list(:item, 2, merchant_id: @merchant2.id)
-    end
-
-    describe "#by_merchant" do
-      it "returns all items associated to a merchant via merchant ID" do
-        items = Item.by_merchant(@merchant1.id)
-
-        expect(items.first).to be_an Item
-        expect(items.count).to eq(3)
-      end
-    end
-  end
 end
